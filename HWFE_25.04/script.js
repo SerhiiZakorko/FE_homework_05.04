af76072be97df43bb925191d2db496ea1e678ccf
SerhiiZakorko <129493695+SerhiiZakorko@users.noreply.github.com>
2023-04-25 19:00:46 +0200

    Add files via upload

---
+++ HWFE_25.04/script.js
@@ -0,0 +1,29 @@
+/* for (let i = 0; i <= 10; i = i + 2) {
+    console.log(i)
+} */
+
+/* for (let i = 55; i >= 20; i = i - 1) {
+    console.log(i)
+} */
+
+/* const numbers = [3, 5, 11, 2, 8, 1, 6]
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+} */
+
+/* const numbers = [3, 5, 11, 2, 8, 1, 6]
+const numbers_squared = []
+for (let i = 0; i < numbers.length; i++) {
+    const number_squared = numbers[i] ** 2
+    numbers_squared.push(number_squared)
+}
+const last_elem = numbers_squared[6]
+console.log(last_elem) */
+
+/* const user = {
+    first_name: 'Ivan',
+    last_name: 'Ivanov',
+    age: 20,
+    salary: 500
+}
+console.log(`${user.first_name} ${user.last_name}. He is ${user.age} years old`) */ 
